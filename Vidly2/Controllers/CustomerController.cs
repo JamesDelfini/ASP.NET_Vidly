fa0f801efe719fbc90a2357964a43f4b751b9692
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 13:45:55 +0800

    Added a Customer Controller and Views (Index, Show) with passing the data's to the View.

---
+++ Vidly2/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+        
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        public ActionResult Show(int? id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
