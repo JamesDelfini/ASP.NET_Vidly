0a007643c723509117c352a30bc87448ce0c44ad
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 14:32:43 +0800

    Added Movies Table, Controller and Views.

---
+++ Vidly2/Models/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime? DatePublish { get; set; }
+        public int Stock { get; set; }
+    }
+}
