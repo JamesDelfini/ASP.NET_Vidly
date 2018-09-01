51963f97a85554bcf126f4d44e564f94a0195392
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 12:29:32 +0800

    Added a Customer Table

---
+++ Vidly2/Migrations/201809010425131_AddCustomerTable.cs
@@ -0,0 +1,27 @@
+namespace Vidly2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomerTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.Int(nullable: false),
+                        isSubscribeNewsLetter = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ Vidly2/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public bool isSubscribeNewsLetter { get; set; }
+    
+    }
+}
