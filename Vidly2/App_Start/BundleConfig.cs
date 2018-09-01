dbddad7a9d18b5d4ef26b52cd11d54682160db07
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 12:10:13 +0800

    Starting Layout Setup

--- Vidly2/App_Start/BundleConfig.cs
+++ Vidly2/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly2
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-simplex-min.css",
                       "~/Content/site.css"));
         }
     }
