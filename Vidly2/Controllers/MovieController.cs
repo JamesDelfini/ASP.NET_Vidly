8f400a00bf1032d1d717c1ef89f90df885bdb74d
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 15:19:09 +0800

    Passing the value of MovieController Data to the View from the Database. Same process in the CustomerController.

--- Vidly2/Controllers/MovieController.cs
+++ Vidly2/Controllers/MovieController.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Vidly2.Models;
+using System.Data.Entity;
+
 namespace Vidly2.Controllers
 {
     public class MovieController : Controller
     {
+        public ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movie
         public ActionResult Index()
         {
-            return View();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+        public ActionResult Show(int? id)
+        {
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movies == null)
+                HttpNotFound();
+
+            return View(movies);
         }
     }
 }
