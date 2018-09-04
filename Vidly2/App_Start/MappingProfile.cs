45bd6b2bc4032e3af970b6d897a2f093302b17a7
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-04 17:25:40 +0800

    Added a AutoMapper Library. It used in API to have DTO (Data Transfer Object) to have consistent property. API Reversioning helps more stable property. API should never receive/return objects.

---
+++ Vidly2/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly2.Dtos;
+using Vidly2.Models;
+
+namespace Vidly2.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ Vidly2/Dtos/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Vidly2.Models;
+
+namespace Vidly2.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "Date")]
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool isSubscribeNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
